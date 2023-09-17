0504ad792e29ee346afb71be5a4cea5222a75d8c
RochaGabriell <rochagabriell.dev@gmail.com>
2023-09-17 13:49:39 -0300

    Up

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
   baseURL: 'http://ws.audioscrobbler.com/2.0/',
   params: {
-    api_key: '010aa297f459645dcd176f4c357c3e5d',
+    api_key: '',
     format: 'json'
   }
 })
